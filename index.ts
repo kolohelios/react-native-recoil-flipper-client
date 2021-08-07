97d5a0161fda34156fb56430834c0817b7c6737f
Jon Edwards <jkedwards@me.com>
2021-08-07 16:28:05 -0700

    pre-alpha: Add type def and some Flipper connection test code

--- index.ts
+++ index.ts
@@ -1,8 +1,36 @@
 import { useEffect } from 'react';
 import { useRecoilSnapshot } from 'recoil';
+import { addPlugin } from 'react-native-flipper';
 
 export const RecoilFlipperClient = () => {
 	const snapshot = useRecoilSnapshot();
+
+	useEffect(() => {
+		addPlugin({
+			getId() {
+				return 'MyFlipperPlugin';
+			},
+			onConnect(connection) {
+				console.log({ connection });
+				// Incoming data from desktop client
+				connection.receive('getData', (data, responder) => {
+					console.log('Incoming data', data);
+					// Respond with some data
+					responder.success({
+						ack: true,
+					});
+				});
+				// Outgoing data to be sent to the desktop client
+				connection.send('newRow', { message: 'Hello' });
+			},
+			onDisconnect() {
+				console.log('disconnected');
+			},
+			runInBackground() {
+				return false;
+			},
+		});
+	}, []);
 
 	useEffect(() => {
 		console.debug('The following atoms were modified:');
